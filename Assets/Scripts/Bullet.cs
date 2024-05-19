976be8cec813646cbd1209c51bc5e5b88b23a4ac
ArminPosadas <132945408+ArminPosadas@users.noreply.github.com>
2024-05-18 19:26:50 -0600

    Ahora si clases terminaras
    earggggg

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public int bulletDamage = 20; // Damage dealt by the bullet
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            Destroy(collision.gameObject);
-            Destroy(gameObject);
+            // Apply damage to the enemy
+            collision.gameObject.GetComponent<Enemy>().TakeDamage(bulletDamage);
         }
-        else
-        {
-            Destroy(gameObject);
-        }
+        // Destroy the bullet regardless of what it collides with
+        Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/Mage.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mage : MonoBehaviour
+{
+    public int health = 20;
+    public float walkSpeed = 6f;
+    public float runSpeed = 12f;
+    public float jumpPower = 7f;
+    public float gravity = 10f;
+}
