4966150b8364f57cd57e483318ed664923c6f83b
ArminPosadas <132945408+ArminPosadas@users.noreply.github.com>
2024-07-25 08:58:31 -0600

    AJAHAHAHAHAHAHAHAH PINCHE PHOTON XD
    inaofgleurkg

--- Assets/Scripts/Mage.cs
+++ Assets/Scripts/Mage.cs
@@ -1,81 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class Mage : FPSMovement
 {
-    public float grabDistance = 2f; 
+    public float grabDistance = 2f;
     public LayerMask grabbableLayer;
-    private Transform grabbedObject; 
+    private Transform grabbedObject;
     private bool isGrabbing = false;
 
-       private void Update()
+    private void Update()
     {
         base.Update();
-
         if (photonView.IsMine)
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
-
                 if (!isGrabbing)
                 {
                     TryGrabObject();
                 }
-
                 else
                 {
                     ReleaseObject();
                 }
+            }
 
-
-                if (isGrabbing && grabbedObject != null)
-                {
-                    UpdateGrabbedObjectPosition();
-                }
+            if (isGrabbing && grabbedObject != null)
+            {
+                UpdateGrabbedObjectPosition();
             }
         }
-      
-        
-
     }
 
-    
-     void TryGrabObject()
+    void TryGrabObject()
     {
         RaycastHit hit;
+
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, grabDistance, grabbableLayer))
         {
             Rigidbody rb = hit.collider.GetComponent<Rigidbody>();
+
             if (rb != null)
             {
                 grabbedObject = rb.transform;
                 isGrabbing = true;
+
+                rb.useGravity = false;
+                rb.constraints = RigidbodyConstraints.FreezeRotation;
             }
         }
     }
 
-    
     void ReleaseObject()
     {
-        grabbedObject = null;
-        isGrabbing = false;
+        if (grabbedObject != null)
+        {
+            Rigidbody rb = grabbedObject.GetComponent<Rigidbody>();
+            
+            if (rb != null)
+            {
+                rb.useGravity = true;
+                rb.constraints = RigidbodyConstraints.None;
+            }
+
+            grabbedObject = null;
+            isGrabbing = false;
+        }
     }
 
-    
     void UpdateGrabbedObjectPosition()
     {
-        
         Vector3 targetPosition = Camera.main.transform.position + Camera.main.transform.forward * grabDistance;
 
-        
         grabbedObject.position = Vector3.Lerp(grabbedObject.position, targetPosition, Time.deltaTime * 10f);
     }
 }
-
-
-
-
-
-
-
